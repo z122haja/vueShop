80b4d2d588ce95662971fefcffa904b524eefce3
周梦奥 <2030385017@qq.com>
2022-10-29 18:23:58 +0800

    完成用户列表功能

--- src/main.js
+++ src/main.js
@@ -10,7 +10,13 @@ import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
 //配置请求的根路径
-axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 设置拦截器 每次请求给header携带token
+axios.interceptors.request.use((config) => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
